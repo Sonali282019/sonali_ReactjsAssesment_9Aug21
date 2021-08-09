92db3c9e91c91d3ac0535f5db05e9f253d83f2b3
Sonali282019 <69837181+Sonali282019@users.noreply.github.com>
2021-08-09 11:08:02 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import './App.css';
+import Home from './components/pages/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import ContactUs from './components/pages/ContactUs';
+import SignUp from './components/pages/SignUp';
+import Marketing from './components/pages/Marketing';
+import Consulting from './components/pages/Consulting';
+import Login from './components/Login';
+import Logout from './components/pages/Logout';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route path='/services' component={Services} />
+        <Route path='/products' component={Products} />
+        <Route path='/contact-us' component={ContactUs} />
+        <Route path='/sign-up' component={SignUp} />
+        <Route path='/marketing' component={Marketing} />
+        <Route path='/logout' component={Logout} />
+        <Login/>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
---
+++ Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../../App.css';
+
+export default function Home() {
+  return (
+    <>
+      <h1 className='home'>React_Asses</h1>
+    </>
+  );
+}
